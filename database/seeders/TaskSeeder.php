d3ca6b419b1955273f750ef433689bde866c20e8
YASUO0715 <yasuo0715niihori@gmail.com>
2021-09-21 08:49:26 +0900

    タスク管理アプリを完了しました

---
+++ database/seeders/TaskSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            [
+                'title' => '',
+                'body' => '',
+            ],
+        ];
+        # DB::table->insertでレコードの登録
+        DB::table('tasks')->insert($param);
+    }
+}
---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>task show</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body>
+    <h1>タスク詳細</h1>
+    <p>
+        <b>【タイトル】</b><br>
+        <b>{{ $task->title }}</b>
+    </p>
+
+    <p>
+        <b>【内容】</b><br>
+        <b>{{ $task->body }}</b>
+    </p>
+    <p>
+        <b></b>
+    </p>
+
+    <div class="button-group">
+        <!-- 商品のidを元に編集ページへ遷移する -->
+        <button onclick="location.href='/tasks/{{ $task->id }}/edit'">編集する</button>
+        <button onclick="location.href='/tasks'">一覧へ戻る</button>
+
+        <form action="/tasks/{{ $task->id }}" method="post">
+            @csrf
+            @method('DELETE')
+            <input type="submit" value="削除する" onclick="if(!confirm('削除しますか？')){return false};">
+        </form>
+    </div>
+
+</body>
+
+</html>
